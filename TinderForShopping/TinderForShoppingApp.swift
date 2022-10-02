04b99630e346c17ea6f2d014d9f8963528f52692
Nikita Arshinov <nikitaarshinov01k@gmail.com>
2022-10-02 19:21:19 +0300

    Initial commit

---
+++ TinderForShopping/TinderForShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderForShoppingApp.swift
+//  TinderForShopping
+//
+//  Created by nikita on 2.10.22.
+//
+
+import SwiftUI
+
+@main
+struct TinderForShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
